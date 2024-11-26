ee2b0eb7ba9141465906a02638479b6dc890b085
Hanyuan Zhao <zhaohy22@mails.tsinghua.edu.cn>
2024-11-26 15:40:35 +0000

    feat: make qemu output the in_asm and specify smp to 1

--- debug.sh
+++ debug.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/bash
 
-$HOME/qemu/build/qemu-system-riscv64 -nographic -machine virt \
+$HOME/qemu/build/qemu-system-riscv64 -nographic -machine virt -smp 1 \
     -m 128M -kernel arch/riscv/boot/Image \
+    -d in_asm -D qemu_exec.log \
     -s -S
+#    -accel tcg,one-insn-per-tb=on \
 #    -plugin $HOME/qemu/build/contrib/plugins/libcache.so -d plugin -D qemu_cache_plugin_log
