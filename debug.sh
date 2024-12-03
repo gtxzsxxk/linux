ce49800b47b0dcaa672b144cd0247517e647628c
Hanyuan Zhao <zhaohy22@mails.tsinghua.edu.cn>
2024-12-03 11:37:48 +0000

    feat: add virtio block device in debug for rootfs

--- debug.sh
+++ debug.sh
@@ -2,6 +2,9 @@
 
 $HOME/qemu/build/qemu-system-riscv64 -nographic -machine virt -smp 1 \
     -m 128M -kernel arch/riscv/boot/Image \
+    -append "root=/dev/vda rw console=ttyS0" \
+    -drive if=none,file=../buildroot-2024.08.2/output/images/rootfs.ext2,format=raw,id=hd0  \
+	-device virtio-blk-device,drive=hd0 \
     -d in_asm -D qemu_exec.log \
     -s -S
 #    -accel tcg,one-insn-per-tb=on \
